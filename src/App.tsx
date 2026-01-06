eb1b4fe340ee5e89afecefc1c21633349bc99bfe
Mateusz Seler <mateusz.seler@gmail.com>
2026-01-06 17:57:24 +0100

    implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type Props = {};
+type State = {
+  key: string | null;
+};
+
+export class App extends React.Component<Props, State> {
+  state: State = {
+    key: null,
+  };
+
+  handelKeyDown = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keydown', this.handelKeyDown);
+  }
+
+  componentWillUnmount(): void {
+    document.addEventListener('keydown', this.handelKeyDown);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.key
+            ? `The last pressed key is [${this.state.key}]`
+            : `Nothing was pressed yet`}
+        </p>
+      </div>
+    );
+  }
+}
